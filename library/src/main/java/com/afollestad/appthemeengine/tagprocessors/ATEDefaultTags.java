68df8a48646781af7809f6f5ab4b4722b6a5c822
Aidan Follestad <drummer.aidan@gmail.com>
2016-01-30 13:35:14 -0600

    Update

--- library/src/main/java/com/afollestad/appthemeengine/tagprocessors/ATEDefaultTags.java
+++ library/src/main/java/com/afollestad/appthemeengine/tagprocessors/ATEDefaultTags.java
@@ -15,11 +15,9 @@ import android.widget.TextView;
 import com.afollestad.appthemeengine.util.ATEUtil;
 import com.afollestad.appthemeengine.util.NestedScrollViewUtil;
 import com.afollestad.appthemeengine.util.RecyclerViewUtil;
-import com.afollestad.appthemeengine.util.SearchViewUtil;
 import com.afollestad.appthemeengine.util.TabLayoutUtil;
 import com.afollestad.appthemeengine.viewprocessors.NestedScrollViewProcessor;
 import com.afollestad.appthemeengine.viewprocessors.RecyclerViewProcessor;
-import com.afollestad.appthemeengine.viewprocessors.SearchViewProcessor;
 import com.afollestad.appthemeengine.viewprocessors.TabLayoutProcessor;
 
 import java.util.HashMap;
@@ -60,11 +58,7 @@ public final class ATEDefaultTags {
 
     @Nullable
     private static HashMap<String, String> get(View forView) {
-        if (ATEUtil.isInClassPath(SearchViewProcessor.MAIN_CLASS) &&
-                SearchViewUtil.isSearchViewAutoComplete(forView)) {
-            // SearchView.AutoComplete is an EditText but we want to theme it like a TextView
-            return getDefaultTextView();
-        } else if (forView instanceof EditText)
+        if (forView instanceof EditText)
             return getDefaultEditText();
         else if (forView instanceof CompoundButton)
             return getDefaultCompoundButton();
